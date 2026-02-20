96ad2ed3d0c6e91673787be226503172db63fec4
kouahodavid6 <kouahodavid6@gmail.com>
2026-02-20 18:47:45 +0000

    Changement de la base url

--- src/api/config.js
+++ src/api/config.js
@@ -1,1 +1,2 @@
-export const API_URL = "https://tropdouxrecup.onrender.com";
+// export const API_URL = "https://tropdouxrecup.onrender.com";
+export const API_URL = "https://tropdoux.msgroupe.tech";
